f55face6dc9f5e5a417767676b2a62cebb17ba05
101shaan <lgs22sisos@lsf.org>
2025-07-20 00:10:28 +0100

    comment out performance module temporarily for core compilation

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
-pub mod lexer;
-pub mod semantic;
 pub mod ast;
+pub mod lexer;
 pub mod parser;
-pub mod visitor;
+pub mod semantic;
 pub mod codegen;
+pub mod visitor;
 pub mod lsp;
+
+/// Performance analysis module - implements contract verification and cost estimation
+// TODO: Re-enable after fixing core compilation
+// pub mod performance;
 
 pub use lexer::{Lexer, Token, TokenType, Position, LexerError};
 pub use ast::{Module, Expr, Stmt, Item, Pattern, Type, Span};
